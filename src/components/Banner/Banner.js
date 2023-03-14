3b40a4418c889ab11573de39b52d78120a095c48
Pedro Martins <pedro_nd5@hotmail.com>
2023-03-13 22:45:35 -0300

    First upload of the project to github

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,9 @@
+import '../../style/components/_Banner.sass'
+
+function Banner() {
+    return(
+        <img src="/images/banner.png" alt="Banner inicial da Organo" srcset="" />
+    );
+}
+
+export default Banner;
