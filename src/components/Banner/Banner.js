f3dcc0a099890ab917919683fe4f5910c5428818
Pedro Martins <pedro_nd5@hotmail.com>
2023-03-15 19:33:21 -0300

    Banner update

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,7 +2,9 @@ import '../../style/components/_Banner.sass'
 
 function Banner() {
     return(
-        <img src="/images/banner.png" alt="Banner inicial da Organo" srcset="" />
+        <header className='banner'>
+            <img src="/images/banner.png" alt="Banner inicial da Organo" srcset="" />
+        </header>
     );
 }
 
